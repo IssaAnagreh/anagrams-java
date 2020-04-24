70f760a24122c04ef6e5622b1769b17f0d77c4c8
IssaAnagreh <isa_anagreh@hotmail.com>
2020-04-24 17:27:05 +0300

    first commit

---
+++ src/main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Anagrams Anagrams = new Anagrams();
+        Anagrams.my_anagrams();
+    }
+
+    public static class Anagrams {
+        static class result {
+            public static String anagrams(String s1, String s2) {
+                // return "YES" if s1 and s2 are anagrams and return "NO" otherwise
+                String output = "YES";
+
+
+                s1 = s1.replaceAll("\\s", "");
+                char[] s1_array = s1.toCharArray();
+                Arrays.sort(s1_array);
+
+                s2 = s2.replaceAll("\\s", "");
+                char[] s2_array = s2.toCharArray();
+                Arrays.sort(s2_array);
+
+                if (!Arrays.equals(s1_array, s2_array)) {
+                    output = "NO";
+                }
+
+                return output;
+            }
+        }
+
+        public void my_anagrams() throws Exception {
+            String s1 = "asas";
+
+            String s2 = "sas";
+
+            String t = result.anagrams(s1, s2);
+
+            // here must have a conflict from user-1
+
+            System.out.println("output original: " + t);
+        }
+    }
+}
